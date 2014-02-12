528dfddf50d76eb206faee374fca9a15e2cc5488
cenderin <ml.alexander87@gmail.com>
2014-02-11 16:47:35 -0800

    new changes

--- encryptor.rb
+++ encryptor.rb
@@ -1,18 +1,28 @@
 class Encryptor 
+	def initialize(default_rotation = 20)
+		@rotation = default_rotation
+	end
 
 	def cipher
-			{ "a" => "n", "b" => "o", "c" => "p", "d" => "q",
-				"e" => "r", "f" => "s", "g" => "t", "h" => "u",
-				"i" => "v", "j" => "w", "k" => "x", "l" => "y",
-				"m" => "z", "n" => "a", "o" => "b", "p" => "c",
-			  "q" => "d", "r" => "e", "s" => "f", "t" => "g",
-			  "u" => "h", "v" => "i", "w" => "j", "x" => "k",
-			  "y" => "l", "z" => "m"  	}
+			k = ('a'..'z').to_a 
+			v = k.to_a.rotate(@rotation)
+			zipped = k.zip(v)
+			Hash[zipped]	
+	end
 
+	def decipher
+			k = ('a'..'z').to_a.reverse
+			v = k.to_a.rotate(-@rotation)
+			zipped = k.zip(v)
+			Hash[zipped]	
 	end
 
 	def encrypt_letter(letter)
 		cipher[letter.downcase]
+	end
+
+	def encrypt_letter_decrypt(letter)
+		decipher[letter.downcase]
 	end
 
 	def encrypt(string)
@@ -22,13 +32,19 @@ class Encryptor
 
   def decrypt(string)
     letters = string.split("")
-    letters.collect { |letter| encrypt_letter(letter) }.join.capitalize
+    letters.collect { |letter| encrypt_letter_decrypt(letter) }.join.capitalize
   end
-
-
 end
 
 
+# require './encryptor'
+# read_file = File.open('secret.txt')
+# word = read_file.read
+# read_file.close
 
+# e = Encryptor.new(20)
+# encrypted_word = e.decrypt(word)
 
-#cipher.key("a")
+# write_file = File.open('secret.txt', 'w+')
+# write_file.write(encrypted_word)
+# write_file.close
---
+++ tictactoe.rb
@@ -0,0 +1,9 @@
+class Board
+	def game
+		grid = {[0,0,0],
+					  [0,0,0],
+					  [0,0,0] }
+			puts grid
+end
+
+
